a43824de8241ff5762c38b050d0be6395b931b5a
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-19 16:35:47 +0300

    Add comments

--- client/client.py
+++ client/client.py
@@ -3,8 +3,10 @@ import socket
 import sys
 import math
 
+# delimiter to differentiate between data and file name
 delimiter = '@@@@@'.encode()
 
+# send a byte array via socket
 def send_bytes(data : bytes, sock : socket):
     totalsent = 0
     while totalsent < len(data):
@@ -14,23 +16,33 @@ def send_bytes(data : bytes, sock : socket):
         totalsent += sent
 
 def main():
+    # read command line arguments
     file_name = sys.argv[1]
     host = sys.argv[2]
     port = int(sys.argv[3])
     timeout = 100
 
+    # create a telnet session for convenice
     with telnetlib.Telnet(host, port, timeout) as session:
+        # get telnet's underlying socket
         sock = session.get_socket()
+        # open the file to send
         with open(file_name, 'rb') as f:
+            # send file name
             send_bytes(file_name.encode(), sock)
+            # send delimiter
             send_bytes(delimiter, sock)
 
+            # read bytes from file
             msg = f.read()
+            
+            # divide the file into portions for better progress control
             portion_size = 2 << 15
             portions = math.ceil(len(msg) / portion_size)
 
             for i in range(portions):
                 send_bytes(msg[i*portion_size:(i+1)*portion_size], sock)    
+                # report current progress
                 print('Sent {:.2%}'.format((i+1) / portions))
 
 
--- server/server.py
+++ server/server.py
@@ -3,34 +3,38 @@ from threading import Thread
 import os
 import sys
 
+# delimiter to differentiate between data and file name
 delimiter = '@@@@@'.encode()
 
-clients = []
-
+# a wrapper for client thread
 class ClientListener(Thread):
     def __init__(self, sock: socket.socket):
         super().__init__(daemon=True)
         self.sock = sock
     
     def _close(self):
-        clients.remove(self.sock)
         self.sock.close()
         print(self.name + ' disconnected')
 
     def run(self):
         all_data = []
         while True:
+            # receiving data in portions
             data = self.sock.recv(1024)
 
-            if data:
-                all_data += list(data)
-            else:
+            if data: # received something
+                all_data += list(data) # append it
+            else: # finish receiving
+                # using the delimiter, split data and file name
                 (file_name, file_data) = ClientListener.split_by_delimiter(bytes(all_data))
 
+                # if delimiter is present
                 if file_name and file_data:
                     print(f'Received {file_name} ({len(file_data)}) bytes')
+                    # get an unused file name
                     file_name = ClientListener.get_unused_file_name(file_name)
                     
+                    # write file to disk
                     with open(file_name, 'wb') as f:
                         f.write(file_data)
  
@@ -42,24 +46,31 @@ class ClientListener(Thread):
     def split_by_delimiter(cls, data : bytes) -> (str, bytes):
         delimiter_len = len(delimiter)
 
+        # searching fot the delimiter in data
         for i in range(len(data) - delimiter_len):
             part = data[i:i+delimiter_len]
+
             if bytearray(part) != bytearray(delimiter):
                 continue
 
+            # split by delimiter
             file_name = str(data[:i].decode())
             file_data = data[i+delimiter_len:]
             return (file_name, file_data)
 
         
+        # report that the format is invalid
         print('Invalid format')
         return (None, None)
 
+    # find a non-used file name
     @classmethod
     def get_unused_file_name(cls, file_name : str) -> str:
+        # if not used at all, return the name itself
         if not os.path.exists('./' + file_name):
             return file_name
 
+        # separate extension
         dot_index = file_name.rfind('.')
         extension = ''
         name = file_name
@@ -69,13 +80,18 @@ class ClientListener(Thread):
             name = file_name[:dot_index]
         
         num = 1
-        file_path = './' + name + '_copy' + str(num) + '.' + extension
+        file_path = ClientListener.get_full_file_name(name, num, extension)
 
+        # trying to find an unused index
         while os.path.exists(file_path):
             num += 1
-            file_path = './' + name + '_copy' + str(num) + '.' + extension
+            file_path = ClientListener.get_full_file_name(name, num, extension)
 
         return file_path
+
+    @classmethod
+    def get_full_file_name(cls, name, num, extension):
+        return './' + name + '_copy' + str(num) + '.' + extension
 
 
 def main():
@@ -85,7 +101,6 @@ def main():
         sock.listen()
         while True:
             con, addr = sock.accept()
-            clients.append(con)
             print(str(addr) + ' connected')
             ClientListener(con).start()
 
